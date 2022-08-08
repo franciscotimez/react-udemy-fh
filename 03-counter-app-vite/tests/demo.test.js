ae6989965c724dd4d9d4e5f928a8dc0ebf23007e
Francisco Timez <franciscotimez@gmail.com>
2022-08-08 13:56:44 -0300

    Demo de pruebas con jest

---
+++ 03-counter-app-vite/tests/demo.test.js
@@ -0,0 +1,13 @@
+
+describe('Pruebas en <DemoComponent />', () => {
+    test('Esta prueba no pueba nada', () => {
+        // Inicializacion
+        const msg1 = 'Esta prueba no pueba nada';
+        // Estimulo
+        const msg2 = 'Estim';
+        // Observar
+        expect(msg1).toBe(msg2);
+
+        ex;
+    });
+});
