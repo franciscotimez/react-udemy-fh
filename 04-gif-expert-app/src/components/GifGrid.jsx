378dec66b640da18beb9246041d21e1b94f8cc05
Francisco Timez <franciscotimez@gmail.com>
2022-08-10 12:05:15 -0300

    Refactor with customHooks

--- 04-gif-expert-app/src/components/GifGrid.jsx
+++ 04-gif-expert-app/src/components/GifGrid.jsx
@@ -1,24 +1,19 @@
-import { useEffect, useState } from "react";
-import { getGifs } from "../helpers/getGifs";
+import { useFetchGifs } from "../hooks/useFetchGifs";
 import { GifItem } from "./GifItem";
-
 
 export const GifGrid = ({ category }) => {
 
-  const [images, setImages] = useState([]);
+  const { images, isLoading } = useFetchGifs(category)
 
-  const getImages = async () => {
-    const newImages = await getGifs(category);
-    setImages(newImages);
-  };
-
-  useEffect(() => {
-    getImages();
-  }, []);
+  console.log(images,isLoading)
 
   return (
     <>
       <h3>{category}</h3>
+      {
+        isLoading &&
+        <h2>Cargando...</h2>
+      }
       <div className="card-grid">
         {
           images.map(image => (
