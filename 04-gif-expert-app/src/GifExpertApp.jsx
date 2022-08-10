45dbd5e0af65d22a528be7034c66f0731c122084
Francisco Timez <franciscotimez@gmail.com>
2022-08-10 10:00:15 -0300

    primer hook, useState

--- 04-gif-expert-app/src/GifExpertApp.jsx
+++ 04-gif-expert-app/src/GifExpertApp.jsx
@@ -1,7 +1,17 @@
-
+import { useState } from "react";
 
 export const GifExpertApp = () => {
+
+  const [categories, setCategories] = useState(['One Punch', 'Dragon Ball']);
+
   return (
-    <div>GifExpertApp</div>
-  )
-}
+    <>
+      <h1>GifExpertApp</h1>
+      <ol>
+        {categories.map(category => {
+          return <li key={category}>{category}</li>;
+        })}
+      </ol>
+    </>
+  );
+};
