6d8e7c4e8389d1793a2f15e6ec71cda853614fc7
Francisco Timez <franciscotimez@gmail.com>
2022-08-24 12:25:47 -0300

    useNaviagte

--- 07-heroes-spa/src/ui/components/NavBar.jsx
+++ 07-heroes-spa/src/ui/components/NavBar.jsx
@@ -1,7 +1,14 @@
-import { Link, NavLink } from 'react-router-dom';
+import { Link, NavLink, useNavigate } from 'react-router-dom';
 
 
 export const NavBar = () => {
+
+  const navigate = useNavigate();
+
+  const onLogout = () => {
+    console.log('Logout');
+    navigate('/login', { replace: true });
+  };
   return (
     <nav className="navbar navbar-expand-sm navbar-dark bg-dark p-2">
 
@@ -38,6 +45,7 @@ export const NavBar = () => {
           </span>
           <button
             className="nav-item nav-link btn"
+            onClick={onLogout}
           >
             LogOut
           </button>
