242ea0cda3ae3396cfc5816e7f63146a04f89ffc
Francisco Timez <franciscotimez@gmail.com>
2022-08-24 11:38:17 -0300

    Inicio heroes SPA

---
+++ 07-heroes-spa/src/heroes/pages/DcPage.jsx
@@ -0,0 +1,8 @@
+
+
+
+export const DcPage = () => {
+  return (
+    <h1>DcPage</h1>
+  )
+}
---
+++ 07-heroes-spa/src/router/AppRouter.jsx
@@ -0,0 +1,19 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+import { LoginPage } from "../auth/pages/LoginPage";
+import { DcPage } from "../heroes/pages/DcPage";
+import { MarvelPage } from "../heroes/pages/MarvelPage";
+
+export const AppRouter = () => {
+  return (
+    <>
+        <Routes>
+            <Route path="marvel" element={<MarvelPage />} />
+            <Route path="dc" element={<DcPage />} />
+            
+            <Route path="login" element={<LoginPage />} />
+
+            <Route path="/" element={<Navigate to="/marvel" />} />
+        </Routes>
+    </>
+  )
+}
