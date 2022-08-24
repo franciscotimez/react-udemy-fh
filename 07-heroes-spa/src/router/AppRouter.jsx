43badd730c16adc06916d7aa9a7a37c4838a7bde
Francisco Timez <franciscotimez@gmail.com>
2022-08-24 12:17:56 -0300

    Make some pages

--- 07-heroes-spa/src/router/AppRouter.jsx
+++ 07-heroes-spa/src/router/AppRouter.jsx
@@ -1,19 +1,16 @@
 import { Navigate, Route, Routes } from "react-router-dom";
-import { LoginPage } from "../auth/pages/LoginPage";
-import { DcPage } from "../heroes/pages/DcPage";
-import { MarvelPage } from "../heroes/pages/MarvelPage";
+
+import { LoginPage } from "../auth";
+import { HeroesRoutes } from "../heroes/routes/HeroesRoutes";
+import { NavBar } from "../ui/components/NavBar";
 
 export const AppRouter = () => {
   return (
     <>
-        <Routes>
-            <Route path="marvel" element={<MarvelPage />} />
-            <Route path="dc" element={<DcPage />} />
-            
-            <Route path="login" element={<LoginPage />} />
-
-            <Route path="/" element={<Navigate to="/marvel" />} />
-        </Routes>
+      <Routes>
+        <Route path="login" element={<LoginPage />} />
+        <Route path="/*" element={<HeroesRoutes />} />
+      </Routes>
     </>
-  )
-}
+  );
+};
