bc62f0b7f149fa47839857779c0274cb2e2f51c3
Francisco Timez <franciscotimez@gmail.com>
2022-08-25 09:38:15 -0300

    Rutas publicas y privadas

--- 07-heroes-spa/src/router/AppRouter.jsx
+++ 07-heroes-spa/src/router/AppRouter.jsx
@@ -1,15 +1,25 @@
-import { Navigate, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 
 import { LoginPage } from "../auth";
 import { HeroesRoutes } from "../heroes/routes/HeroesRoutes";
-import { NavBar } from "../ui/components/NavBar";
+import { PrivateRoute } from "./PrivateRoute";
+import { PublicRoute } from "./PublicRoute";
 
 export const AppRouter = () => {
   return (
     <>
       <Routes>
-        <Route path="login" element={<LoginPage />} />
-        <Route path="/*" element={<HeroesRoutes />} />
+        <Route path="login" element={
+          <PublicRoute>
+            <LoginPage />
+          </PublicRoute>
+        } />
+
+        <Route path="/*" element={
+          <PrivateRoute>
+            <HeroesRoutes />
+          </PrivateRoute>
+        } />
       </Routes>
     </>
   );
---
+++ 07-heroes-spa/src/router/PublicRoute.jsx
@@ -0,0 +1,13 @@
+import { useContext } from "react";
+import { Navigate } from 'react-router-dom';
+
+import { AuthContext } from "../auth";
+
+export const PublicRoute = ({ children }) => {
+
+    const { authState: { logged } } = useContext(AuthContext);
+
+    return (!logged)
+        ? children
+        : <Navigate to="/" />;
+};
