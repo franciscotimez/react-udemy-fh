cb4e61aed4c1585b3953e6e6579e1ac324a48431
Francisco Timez <franciscotimez@gmail.com>
2022-08-25 09:14:31 -0300

    login in localStorage

--- 07-heroes-spa/src/auth/context/AuthProvider.jsx
+++ 07-heroes-spa/src/auth/context/AuthProvider.jsx
@@ -3,25 +3,33 @@ import { types } from '../types/types';
 import { AuthContext } from './AuthContext';
 import { authReducer } from './authReducer';
 
-const initialState = {
-    logged:false
-}
+const init = () => {
+    const user = JSON.parse(localStorage.getItem('user'));
+
+    return {
+        logged: !!user,
+        user
+    };
+};
 
 export const AuthProvider = ({ children }) => {
 
-    const [authState, dispatch] = useReducer(authReducer, initialState);
+    const [authState, dispatch] = useReducer(authReducer, {}, init);
 
     const login = (name = '') => {
+
+        const user = {
+            id: 'abc',
+            name
+        };
         const action = {
             type: types.login,
-            payload: {
-                id: '12132',
-                name
-            }
-        }
+            payload: user
+        };
 
-        dispatch(action)
-    }
+        localStorage.setItem('user', JSON.stringify(user));
+        dispatch(action);
+    };
 
     return (
         <AuthContext.Provider value={{
