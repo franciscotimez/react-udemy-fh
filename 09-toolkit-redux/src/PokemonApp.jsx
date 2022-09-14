5bf751dba00f7a680a98f53ef1ab20ca30194e35
Francisco Timez <franciscotimez@gmail.com>
2022-09-14 18:15:26 -0300

    Uso de Axios

--- 09-toolkit-redux/src/PokemonApp.jsx
+++ 09-toolkit-redux/src/PokemonApp.jsx
@@ -1,25 +1,40 @@
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { getPokemons } from "./store/pokemon/thunks";
 
 export const PokemonApp = () => {
 
   const dispatch = useDispatch();
+  const { pokemons, isLoading, nextPage } = useSelector((state) => state.pokemons);
 
   useEffect(() => {
     dispatch(getPokemons());
-
   }, []);
 
   return (
     <>
       <h1>PokemonApp</h1>
       <hr />
+      <span>
+        {isLoading ? "Cargando..." : "Ready"}
+      </span>
       <ul>
-        <li>Hola</li>
-        <li>Hola</li>
-        <li>Hola</li>
+        {
+          !isLoading &&
+          pokemons.map(({ name }) => (
+            <li key={name}>{name}</li>
+          ))
+        }
       </ul>
+      <span>
+        page: {nextPage - 1}
+      </span>
+      <button
+        disabled={isLoading}
+        onClick={() => dispatch(getPokemons(nextPage))}
+      >
+        Next
+      </button>
     </>
   );
 };
--- 09-toolkit-redux/src/store/pokemon/thunks.js
+++ 09-toolkit-redux/src/store/pokemon/thunks.js
@@ -1,3 +1,4 @@
+import { pokemonApi } from "../../apis/pokemonApi";
 import { setPokemons, startLoadingPokemons } from "./pokemonSlice";
 
 
@@ -5,9 +6,10 @@ export const getPokemons = (page = 0) => {
     return async (dispatch, getState) => {
         dispatch(startLoadingPokemons());
 
-        const resp = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=10&offset=${page * 10}`);
-        const data = await resp.json();
-        
+        // const resp = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=10&offset=${page * 10}`);
+        // const data = await resp.json();
+        const { data } = await pokemonApi.get(`/pokemon?limit=10&offset=${page * 10}`)
+
         dispatch(setPokemons({
             nextPage: page + 1,
             pokemons: data.results
