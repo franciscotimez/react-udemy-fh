c1be9d1cb5077772cec98f55654067d9e63c71e3
Francisco Timez <franciscotimez@gmail.com>
2022-09-17 20:54:41 -0300

    Convert logic checkAuth in useCheckAuth customHook

--- 08-journal-app/src/router/AppRouter.jsx
+++ 08-journal-app/src/router/AppRouter.jsx
@@ -1,27 +1,12 @@
-import { onAuthStateChanged } from "firebase/auth";
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
 import { Route, Routes } from "react-router-dom";
 import { AuthRoutes } from "../auth/routes/AuthRoutes";
-import { FirebaseAuth } from "../firebase/config";
+import { useCheckAuth } from "../hooks/useCheckAuth";
 import { JournalRoutes } from "../journal/routes/JournalRoutes";
-import { login, logout } from "../store/auth/authSlice";
 import { CheckingAuth } from "../ui";
 
 export const AppRouter = () => {
 
-  const { status } = useSelector(state => state.auth);
-
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    onAuthStateChanged(FirebaseAuth, async (user) => {
-      if (!user) return dispatch(logout());
-
-      const { uid, email, displayName, photoURL } = user;
-      dispatch(login({ uid, email, displayName, photoURL }));
-    });
-  }, []);
+  const { status } = useCheckAuth();
 
   if (status === "auth-checking") {
     return <CheckingAuth />;
