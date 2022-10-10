6e595ab31b969a3af9c4aacfaee9d022205b7c14
Francisco Timez <franciscotimez@gmail.com>
2022-10-10 12:20:34 -0300

    Fix error serializable date

--- 10-calendar/src/store/store.js
+++ 10-calendar/src/store/store.js
@@ -5,5 +5,8 @@ export const store = configureStore({
   reducer: {
     ui: uiSlice.reducer,
     calendar: calendarSlice.reducer,
-  }
+  },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+    serializableCheck: false
+  })
 });
