6384f8a2c0c88563f5f36e666fabc9eb5a63120a
Francisco Timez <franciscotimez@gmail.com>
2022-10-11 20:33:48 -0300

    Inicio calendar-backend

---
+++ 11-calendar-backend/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+require('dotenv').config();
+// Crear el servidor de express
+
+const app = express();
+
+// Directorio Publico
+app.use(express.static('public'));
+
+// Rutas
+// app.get('/', (req, res) => {
+
+//   console.log("Holis");
+//   res.json({
+//     ok: true,
+//   });
+// });
+
+// Escuchar peticiones
+app.listen(process.env.PORT, () => {
+  console.log(`Servidor escuchando en el puerto: ${process.env.PORT}`);
+});
