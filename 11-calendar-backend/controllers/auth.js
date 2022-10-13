f355937f642118f66d7f4210a75c289cd8c5cc14
Francisco Timez <franciscotimez@gmail.com>
2022-10-13 10:54:09 -0300

    Validaciones con express-validator

--- 11-calendar-backend/controllers/auth.js
+++ 11-calendar-backend/controllers/auth.js
@@ -1,16 +1,19 @@
 const { response } = require('express');
-
+const { validationResult } = require('express-validator');
 const createUser = (req, res = response) => {
 
   const { name, email, password } = req.body;
 
-  if (name.length < 5) {
+  // Manejo de errores
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
     return res.status(400).json({
       ok: false,
-      msg: "El nombre debe tener mas de 5 letras"
+      errors: errors.mapped()
     });
   }
-  res.json({
+
+  res.status(201).json({
     ok: true,
     msg: 'registro',
     name,
@@ -20,6 +23,15 @@ const createUser = (req, res = response) => {
 };
 
 const loginUser = (req, res = response) => {
+  // Manejo de errores
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({
+      ok: false,
+      errors: errors.mapped()
+    });
+  }
+
   const { email, password } = req.body;
 
   res.json({
--- 11-calendar-backend/routes/auth.js
+++ 11-calendar-backend/routes/auth.js
@@ -1,13 +1,26 @@
 const { Router } = require('express');
-
+const { check } = require('express-validator');
 const router = Router();
 
 const { createUser, loginUser, renewToken } = require('../controllers/auth');
 
 
-router.post('/', loginUser);
+router.post(
+  '/new',
+  [ // Middlewares
+    check('name', 'El nombre es obligatorio').not().isEmpty(),
+    check('email', 'El email es obligatorio').isEmail(),
+    check('password', 'El password debe ser de mas de 6 caracteres').isLength({ min: 6 }),
+  ],
+  createUser);
 
-router.post('/new', createUser);
+router.post(
+  '/',
+  [ // Middlewares
+    check('email', 'El email es obligatorio').isEmail(),
+    check('password', 'El password debe ser de mas de 6 caracteres').isLength({ min: 6 }),
+  ],
+  loginUser);
 
 router.get('/renew', renewToken);
 
