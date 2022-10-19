8fcfbc2a02792e43c171d308ca56de8920dc7097
Francisco Timez <franciscotimez@gmail.com>
2022-10-19 17:10:38 -0300

    Create model user and save in DB

--- 11-calendar-backend/controllers/auth.js
+++ 11-calendar-backend/controllers/auth.js
@@ -1,15 +1,23 @@
 const { response } = require('express');
+const User = require('../models/User');
 
-const createUser = (req, res = response) => {
-  const { name, email, password } = req.body;
+const createUser = async (req, res = response) => {
+  // const { name, email, password } = req.body;
+  try {
+    const usuario = new User(req.body);
+    await usuario.save();
 
-  res.status(201).json({
-    ok: true,
-    msg: 'registro',
-    name,
-    email,
-    password
-  });
+    res.status(201).json({
+      ok: true,
+      msg: 'registro',
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      ok: false,
+      msg: 'Por favor hable con el administrator',
+    });
+  }
 };
 
 const loginUser = (req, res = response) => {
