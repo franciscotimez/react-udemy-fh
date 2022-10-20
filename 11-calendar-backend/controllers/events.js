c49b40cbc8c3ba9ad805a705daa9e95e60c7bbfe
Francisco Timez <franciscotimez@gmail.com>
2022-10-20 11:47:14 -0300

    Update event

--- 11-calendar-backend/controllers/events.js
+++ 11-calendar-backend/controllers/events.js
@@ -32,10 +32,47 @@ const createEvent = async (req, res = response) => {
 };
 
 const updateEvent = async (req, res = response) => {
-  res.json({
-    ok: true,
-    msg: 'updateEvent',
-  });
+
+  const eventId = req.params.id;
+  const uid = req.uid;
+
+  try {
+
+    const event = await Event.findById(eventId);
+
+    if (!event) {
+      return res.status(404).json({
+        ok: false,
+        msg: 'El evento No existe.',
+      });
+    }
+
+    if (event.user.toString() !== uid) {
+      return res.status(401).json({
+        ok: false,
+        msg: 'No tiene permiso de hacer esta accion.',
+      });
+    }
+
+    const newEvent = {
+      ...req.body,
+      user: uid
+    };
+
+    const eventUpdated = await Event.findByIdAndUpdate(eventId, newEvent, { new: true });
+
+    res.status(200).json({
+      ok: true,
+      event: eventUpdated
+    });
+
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      ok: false,
+      msg: 'Por favor hable con el administrator',
+    });
+  }
 };
 
 const deleteEvent = async (req, res = response) => {
