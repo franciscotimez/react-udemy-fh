c75ff39bb2a058e5a0d9bc13598082a521e262fd
Francisco Timez <franciscotimez@gmail.com>
2022-10-19 21:00:17 -0300

    JWT!!

--- 11-calendar-backend/controllers/auth.js
+++ 11-calendar-backend/controllers/auth.js
@@ -1,6 +1,7 @@
 const { response } = require('express');
 const bcrypt = require('bcryptjs');
 const User = require('../models/User');
+const { generateJWT } = require('../helpers/jwt');
 
 const createUser = async (req, res = response) => {
   const { email, password } = req.body;
@@ -22,10 +23,15 @@ const createUser = async (req, res = response) => {
 
     await usuario.save();
 
+    // Generar JWT
+    const token = await generateJWT(usuario.id, usuario.name);
+
     res.status(201).json({
       ok: true,
       uid: usuario.id,
-      name: usuario.name
+      name: usuario.name,
+      token
+
     });
   } catch (error) {
     console.log(error);
@@ -58,11 +64,13 @@ const loginUser = async (req, res = response) => {
     }
 
     // Generar JWT
+    const token = await generateJWT(usuario.id, usuario.name);
 
-    res.status(201).json({
+    res.json({
       ok: true,
       uid: usuario.id,
-      name: usuario.name
+      name: usuario.name,
+      token
     });
   } catch (error) {
     console.log(error);
